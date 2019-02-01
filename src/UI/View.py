92e30a00e23dbb61d7a44bb6b980b067b6804869
alberto <albe.carde@gmail.com>
2019-02-01 08:37:24 +0000

    Added application icon

--- src/UI/View.py
+++ src/UI/View.py
@@ -28,6 +28,8 @@ class View():
         # Create main window
         self.mainWindow = tk.Tk()
         self.mainWindow.title(APP_NAME)
+        img = tk.Image("photo", file=currentdir + '/assets/icon.png')
+        self.mainWindow.tk.call('wm','iconphoto',self.mainWindow._w,img)
         self.mainWindow.protocol("WM_DELETE_WINDOW", self.on_close_event)
         self.mainWindow.geometry("1024x600")
         # Define the app menu
