920635726294b518a49bed9dec621f2d58be0c73
Alberto Cardellini <albe.carde@gmail.com>
2020-08-02 17:51:58 +0100

    Fixed import order

--- tradingmate/ui/gtk/__init__.py
+++ tradingmate/ui/gtk/__init__.py
@@ -1,8 +1,8 @@
+from .message_dialog import MessageDialog  # NOQA # isort:skip
 from .add_trade_window import AddTradeWindow  # NOQA # isort:skip
 from .confirm_dialog import ConfirmDialog  # NOQA # isort:skip
 from .explore_markets_window import ExploreMarketsWindow  # NOQA # isort:skip
 from .log_window import LogWindow  # NOQA # isort:skip
-from .message_dialog import MessageDialog  # NOQA # isort:skip
 from .portfolio_page import PortfolioPage  # NOQA # isort:skip
 from .settings_window import SettingsWindow  # NOQA # isort:skip
 from .ui_handler import UIHandler  # NOQA # isort:skip
